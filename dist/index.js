7b601ed9e53695b4be6a4e251e4d5a2dfc6a766c
Jim Billinger <jamesbillinger@gmail.com>
2019-01-24 13:52:09 -0600

    commit

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,18 @@ var _filepreview = require('./filepreview');
 
 var _filepreview2 = _interopRequireDefault(_filepreview);
 
+var _multer = require('multer');
+
+var _multer2 = _interopRequireDefault(_multer);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var storage = _multer2.default.diskStorage({});
+(0, _multer2.default)({});
+var upload = (0, _multer2.default)({
+  storage: storage,
+  limits: { fieldSize: 25 * 1024 * 1024 }
+});
 
 global.log = _tracer2.default.console().log;
 global.info = _tracer2.default.console().info;
@@ -23,12 +34,36 @@ global.error = _tracer2.default.console().error;
 
 var app = (0, _express2.default)();
 
-app.post('preview', function (req, res, next) {
+app.post('/preview', upload.any(), function (req, res, next) {
   log('here');
+  var file = (req.files || [])[0];
+  if (file) {
+    _filepreview2.default.generate(file.path, '/tmp/preview.png', function (err) {
+      if (err) {
+        log(err);
+        res.status(500);
+        res.json({ error: err });
+      } else {
+        res.sendFile('/tmp/preview.png');
+      }
+    });
+  } else {
+    res.status(500);
+    res.json({ error: 'No File' });
+  }
+});
+app.get('/test', function (req, res, next) {
+  _filepreview2.default.generate('pdf-test.pdf', 'output.png', function (err) {
+    if (err) {
+      log(err);
+      res.status(500);
+      res.json({ error: err });
+    } else {
+      res.json({ success: true });
+    }
+  });
 });
 
 var server = app.listen(3010);
 log('filepreview server bound to port 3010');
-_filepreview2.default.generate('pdf-test.pdf', 'output.png', function (err) {
-  log(err);
-});
+/**/
